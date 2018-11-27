073e3481ddd01c7969a2d9581453f883e418725c
Alistair Blake <consultations@me.com>
2018-11-27 11:02:47 -0800

    updates made

--- 01-smacss-media-queries/challenges/challenges-01.test.js
+++ 01-smacss-media-queries/challenges/challenges-01.test.js
@@ -9,46 +9,53 @@ Then, write a function named speaker that takes in a string and a callback funct
 ------------------------------------------------------------------------------------------------ */
 
 const greeting = (word) => {
-  // Solution code here...
+  return word.toUpperCase();
 }
 
 const speaker = (message, callback) => {
-  // Solution code here...
+  return callback(message);
 }
-
+speaker("Hello", greeting);
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
-
+ 
 Write a function named addValues that takes in an array and a value and pushes the value into the array. This function does not need a return statement.
-
+ 
 Then, write a function named addNumbers that takes in four arguments:
   - A number to be added to an array
   - An array into which the number should be added
   - The number of times the number should be added
   - A callback function to use to add the numbers to the array (Hint: you already defined it)
-
+ 
 Within the addNumbers function, invoke the callback function as many times as necessary, based on the third argument of the addNumbers function.
-
+ 
 Return the modified array.
 ------------------------------------------------------------------------------------------------ */
 
 const addValues = (arr, value) => {
-  // Solution code here...
+  function addValues() {
+    new value.push(this);
+  }
+
 }
 
 const addNumbers = (num, arr, times, callback) => {
-  // Solution code here...
-}
+  function addNumbers() {
+    callback(times);
+  }
+
+};
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
-
+ 
 Write a function named removeOne that takes in a number and an array. If the number divided by three has a remainder of two, pop one element off of the array.
-
+ 
 Hint: you may want to look into the modulo operation.
-
+ 
 Then, write a function named removeElements that takes in an array and a callback. This function should use a for loop to iterate over the array and invoke the callback once for each element in the array.
-
+ 
 Return the modified array.
 ------------------------------------------------------------------------------------------------ */
 
@@ -62,7 +69,7 @@ const removeElements = (arr, callback) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
-
+ 
 Write a function named removeWithForEach that produces the same output as challenge 3, but uses forEach.
 ------------------------------------------------------------------------------------------------ */
 
@@ -72,11 +79,11 @@ const removeWithForEach = (arr, callback) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
-
+ 
 Write a function named removeWithAnon that produces the same output as challenges 3 and 4.
-
+ 
 This function should use forEach again, but rather than taking in a callback as an argument, define an anonymous function as the argument to forEach.
-
+ 
 This anonymous function should accept up to three arguments: the element, the index, and the array.
 ------------------------------------------------------------------------------------------------ */
 
@@ -86,9 +93,9 @@ const removeWithAnon = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
-
+ 
 Write a function named createList that takes in an array of the current store intentory.
-
+ 
 The inventory is formatted like this:
 [
   { name: 'apples', available: true },
@@ -97,7 +104,7 @@ The inventory is formatted like this:
   { name: 'bananas', available: true },
   { name: 'blueberries', available: false }
 ]
-
+ 
 This function should use forEach to populate your grocery list based on the store's inventory. If the item is available, add it to your list. Return the final list.
 ------------------------------------------------------------------------------------------------ */
 
@@ -107,15 +114,15 @@ const createList = (availableItems) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7
-
+ 
 Write a function named fizzbuzz that takes in an array of numbers.
-
+ 
 Iterate over the array using forEach to determine the output based on several rules:
   - If a number is divisible by 3, add the word "Fizz" to the output array.
   - If the number is divisible by 5, add the word "Buzz" to the output array.
   - If the number is divisible by both 3 and 5, add the phrase "Fizz Buzz" to the output array.
   - Otherwise, add the number to the output array.
-
+ 
 Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
@@ -125,13 +132,13 @@ const fizzbuzz = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
-
+ 
 All the code below will verify that your functions are working to solve the challenges.
-
+ 
 DO NOT CHANGE any of the below code.
-
+ 
 Run your tests from the console: jest challenges-01.test.js
-
+ 
 ------------------------------------------------------------------------------------------------ */
 
 describe('Testing challenge 1', () => {
